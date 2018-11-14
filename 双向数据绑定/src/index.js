c3d5a8f58f918cf362912eff018d2473921c9f5c
yangsai <2471776255@qq.com>
2018-11-14 22:15:16 +0800

    双向数据绑定，组件传值，笔记

---
+++ 双向数据绑定/src/index.js
@@ -0,0 +1,11 @@
+// 1. 导入包
+// import React, {Component} from 'react'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// import BindEvent from '@/components/BindInputValue'
+import CmtList from '@/components/CmtList'
+// 3. 调用 render 函数渲染
+ReactDOM.render(<div>
+  <CmtList></CmtList>
+</div>, document.getElementById('app'))
